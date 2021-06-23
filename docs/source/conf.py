af29d51afb5982a933843a438574bc5dc873999f
Charlotte Mason <charlotte.nosam@gmail.com>
2021-06-23 13:28:07 -0400

    mock py21cmfast

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = []
+MOCK_MODULES = ['py21cmfast']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
