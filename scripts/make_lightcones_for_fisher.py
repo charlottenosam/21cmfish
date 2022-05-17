e4b486542b0f51494cf9438508efed536b7dddb3
Charlotte Mason <charlotte.nosam@gmail.com>
2022-05-17 14:44:27 +0200

    use initial_conditions

--- scripts/make_lightcones_for_fisher.py
+++ scripts/make_lightcones_for_fisher.py
@@ -288,7 +288,7 @@ else:
                                         max_redshift = max_redshift,
                                         lightcone_quantities=lightcone_quantities,
                                         global_quantities=global_quantities,
-                                        # init_box = initial_conditions,
+                                        init_box = initial_conditions,
                                         user_params  = user_params,
                                         flag_options = flag_options,
                                         astro_params = astro_params_run_all[astro_params_key],
